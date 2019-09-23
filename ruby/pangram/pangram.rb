db75ad87bbddd0340f87b756e41a6c8165b21ef6
Ian Johnson <ijohnson@covermymeds.com>
2019-09-23 09:34:27 -0400

    Update ruby pangram exercise

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,16 +1,10 @@
 class Pangram
   def self.pangram?(sentence)
-    letter_counts = create_count
+    letter_counts = ('a'..'z').zip([0] * 26).to_h
     sentence.downcase
             .chars
             .select { |c| c =~ /[a-z]/ }
             .each { |c| letter_counts[c] += 1 }
     letter_counts.none? { |k,v| v == 0 }
-  end
-
-  def self.create_count
-    counts = {}
-    ('a'..'z').each { |c| counts[c] = 0 }
-    counts
   end
 end
