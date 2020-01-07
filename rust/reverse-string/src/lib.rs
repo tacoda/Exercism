2691f5f69fb544d5620b5e6badaa09419f52d02e
Ian Johnson <ijohnson@covermymeds.com>
2020-01-07 15:16:13 -0500

    Update reverse string exercise to make the code more readable

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,6 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    // input.chars().rev().collect()
-    UnicodeSegmentation::graphemes(input, true).rev().collect()
+    input.graphemes(true).rev().collect()
 }
