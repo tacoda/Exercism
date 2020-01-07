ad95073aded3b71ba3b03724960ceb80afb6d8c7
Ian Johnson <ijohnson@covermymeds.com>
2020-01-07 14:03:03 -0500

    Complete rust reverse string exercise

---
+++ rust/reverse-string/src/lib.rs
@@ -0,0 +1,6 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn reverse(input: &str) -> String {
+    // input.chars().rev().collect()
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
+}
